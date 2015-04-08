f0542330b3073a4679cf1c71e8315d212692b802
sootn <encrypt.wave@gmail.com>
2015-04-08 21:34:39 +0200

    Z index fix

--- showcase.js
+++ showcase.js
@@ -7,12 +7,14 @@ function route(e) {
     if (location.hash) {
         if (last_slide) {
             last_slide.style.opacity = 0;
+            last_slide.style["z-index"] = 1;
             last_link.style["background-color"] = "";
         }
         var without_hash = location.hash.substring(1, location.hash.length);
         var selector = ".showcase > article[id=\"" + without_hash + "\"]";
         var current_slide = document.querySelector(selector);
         current_slide.style.opacity = 1;
+        current_slide.style["z-index"] = 10;
         last_slide = current_slide;
         selector = ".projectlist a[href=\"" + location.hash + "\"]";
         var current_link = document.querySelector(selector);
